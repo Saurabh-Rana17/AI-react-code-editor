035317c6c4240f9842b6a4e72c6c36dc145b0771
saurabh-rana17 <saurabhrana200317@gmail.com>
2024-04-05 09:51:07 +0530

    changed snippet

--- src/constants/AceSnippet.js
+++ src/constants/AceSnippet.js
@@ -1,7 +1,4 @@
 export const AceSnippet = [
-    { caption: "foo", value: "foo", meta: "Custom" },
-    { caption: "bar", value: "bar", meta: "Custom" },
-    { caption: "baz", value: "baz", meta: "Custom" },
     {
         caption: "for loop",
         value: "for (var i = 0; i < length; i++) {\n\t// Your code here\n}",

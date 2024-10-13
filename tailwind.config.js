17e65eda762dce4eb0dd13bfab92c3ab7d87532f
saurabh-rana17 <saurabhrana200317@gmail.com>
2024-10-13 15:09:40 +0530

    favicon updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",
-    "node_modules/flowbite-react/lib/esm/**/*.js",
+const flowbite = require("flowbite-react");
 
-  ],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [
-    require("flowbite/plugin"),
-
-  ],
+  plugins: [],
 };
